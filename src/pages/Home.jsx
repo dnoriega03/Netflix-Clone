c415ebb639d8bfd848d295944513b06ca6951a14
Diana Noriega <dnoriega0104@gmail.com>
2026-05-13 10:34:46 -0500

    Sección populares dinámica con TMDB

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,31 +25,15 @@ function Home() {
         fetchUrl={`/trending/all/week?api_key=${API_KEY}`}
       />
 
-      <Row title="Top Rated" fetchUrl={`/movie/top_rated?api_key=${API_KEY}`} />
+      <Row
+        title="Top Rated"
+        fetchUrl={`/movie/top_rated?api_key=${API_KEY}`}
+      />
 
-      <div className="movieRow">
-        <h2>Populares en Netflix</h2>
-
-        <div className="row__posters">
-          <img
-            className="row__poster"
-            src="https://image.tmdb.org/t/p/w300/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg"
-            alt=""
-          />
-
-          <img
-            className="row__poster"
-            src="https://image.tmdb.org/t/p/w300/9O1Iy9od7l2YH6W4lI0h9C4Cj9d.jpg"
-            alt=""
-          />
-
-          <img
-            className="row__poster"
-            src="https://image.tmdb.org/t/p/w300/r7XifzvtezNt31ypvsmb6Oqxw49.jpg"
-            alt=""
-          />
-        </div>
-      </div>
+      <Row
+        title="Populares en Netflix"
+        fetchUrl={`/discover/tv?api_key=${API_KEY}&with_networks=213`}
+      />
     </div>
   );
 }
